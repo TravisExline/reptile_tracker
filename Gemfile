60bc27c85b8daf5fb760df56a8b42889f639a23d
Travis Exline <t.exline22@gmail.com>
2019-08-06 18:27:12 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'rails'
+
+# Specify your gem's dependencies in reptile_tracker.gemspec
+gemspec
---
+++ lib/reptile_tracker.rb
@@ -0,0 +1,6 @@
+require "reptile_tracker/version"
+
+module ReptileTracker
+  class Error < StandardError; end
+  # Your code goes here...
+end
